d2cfc69294ddbf9c835bd19a1fe067f6074d4aaf
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-04-20 12:30:19 +0300

    Change /user GET route

--- controllers/user/get.js
+++ controllers/user/get.js
@@ -6,35 +6,43 @@ const getUserObject = require('../../utils/getUserObject');
 const getMatchRatios = require('../../utils/getMatchRatios');
 
 module.exports = (req, res) => {
-  if (!req.query || !req.query.id || !req.query.user)
+  if (!req.query || !req.query.id || !req.query.type)
     return res.status(400).json({ error: "bad request" });
 
-  User.findById(mongoose.Types.ObjectId(req.query.id), (err, req_user) => {
-    if (err || !req_user) return res.status(500).json({ error: "Mongo Error: " + err });
-    
-    User.findById(mongoose.Types.ObjectId(req.query.user), (err, user) => {
+  if (req.query.type == "match") {
+    User.findById(mongoose.Types.ObjectId(req.query.id), (err, req_user) => {
       if (err || !req_user) return res.status(500).json({ error: "Mongo Error: " + err });
-
-      const new_user = getUserObject(user);
-
-      getMatchRatios({
-        option: "get compatibity",
-        user_one: {
-          sign: req_user.sign,
-          mars_sign: req_user.mars_sign,
-          venus_sign: req_user.venus_sign
-        },
-        user_two: {
-          sign: user.sign,
-          mars_sign: user.mars_sign,
-          venus_sign: user.venus_sign
-        }
-      }, (err, matching_ratio) => {
-        if (err) return res.status(500).json({ error: err });
-        new_user.matching_ratio = matching_ratio;
+      
+      User.findById(mongoose.Types.ObjectId(req.query.user), (err, user) => {
+        if (err || !req_user) return res.status(500).json({ error: "Mongo Error: " + err });
   
-        return res.status(200).json({ user: new_user });
+        const new_user = getUserObject(user);
+  
+        getMatchRatios({
+          option: "get compatibity",
+          user_one: {
+            sign: req_user.sign,
+            mars_sign: req_user.mars_sign,
+            venus_sign: req_user.venus_sign
+          },
+          user_two: {
+            sign: user.sign,
+            mars_sign: user.mars_sign,
+            venus_sign: user.venus_sign
+          }
+        }, (err, matching_ratio) => {
+          if (err) return res.status(500).json({ error: err });
+          new_user.matching_ratio = matching_ratio;
+    
+          return res.status(200).json({ user: new_user });
+        });
       });
     });
-  });
+  } else {
+    User.findById(mongoose.Types.ObjectId(req.query.id), (err, user) => {
+      if (err || !req_user) return res.status(500).json({ error: "Mongo Error: " + err });
+
+      return res.status(200).json({ user: getUserObject(user) });
+    });
+  }
 }
