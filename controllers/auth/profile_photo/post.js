5f7b4149e3e08c860a6bab89627c4f3c89c43120
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-05-19 13:46:07 +0300

    Update image quality tools

--- controllers/auth/profile_photo/post.js
+++ controllers/auth/profile_photo/post.js
@@ -17,13 +17,21 @@ module.exports = (req, res) => {
     if (user.profile_photo_list.length > 5)
       return res.status(400).json({ error: "user already have 6 photos" });
 
+    const image_path = "./public/res/uploads/" + req.file.filename;
+
+    const image = await jimp.read(image_path);
+    const image_quality = Math.max(250000 * 100 / req.file.size, 10);
+
+    await image.quality(image_quality);
+    await image.writeAsync(image_path);
+
     req.cloudinary.v2.uploader.upload(
       "./public/res/uploads/" + req.file.filename,
       {
         public_id: "astro/profile_photo/" + req.file.filename,
         format: "JPG",
         secure: true,
-        quality: "auto:low"
+        quality: 100
       },
       (err, result) => {
         if (err) return res.status(400).json({ error: err });
